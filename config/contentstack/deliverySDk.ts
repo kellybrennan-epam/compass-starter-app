33116f9ab6aebd072bcc2f1f9ab86273f41b53fd
Kelly Brennan <kelly_brennan@epam.com>
2024-11-13 13:23:48 -0500

    Fixing build error

--- config/contentstack/deliverySDk.ts
+++ config/contentstack/deliverySDk.ts
@@ -24,8 +24,8 @@ ContentstackLivePreview.init({
     },
     stackSdk: Stack.config as any, // once type definition is availble from SDK any need to be replaced with IStackSdk
     ssr: false,
-	enable: true,
-	mode: "builder",
+    enable: true,
+    mode: "builder"
 })
 
 export const onEntryChange = ContentstackLivePreview.onEntryChange
