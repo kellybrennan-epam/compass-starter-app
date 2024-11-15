ea5f017179a2dcde0c1275a6e63fe04fc66704b9
Kelly Brennan <kelly_brennan@epam.com>
2024-11-15 10:57:08 -0500

    edge configuration

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,20 +1,29 @@
 import Personalize from '@contentstack/personalize-edge-sdk'
 export default async function handler (request, context) {
     const parsedUrl = new URL (request.url)
+    const pathname = parsedUrl.pathname
+    // exclude Next.js asset calls so that only page requests are processed
+    if (['_next', 'favicon.ico'].some((path) => pathname.includes(path))) {
+        return fetch(request)
+    }
     // reset the SDK to remove any existing context
     Personalize.reset()
+    // set a custom edge API URL
     if (context.env.CONTENTSTACK_PERSONALIZE_EDGE_API_URL) {
         Personalize.setEdgeApiUrl(context.env.CONTENTSTACK_PERSONALIZE_EDGE_API_URL)
     }
+    // Initialize the SDK and pass the request as well
     await Personalize.init(context.env.CONTENTSTACK_PERSONALIZE_PROJECT_UID, {
         request
     })
-    // get the variant parameter from the SDK
     const variantParam = Personalize.getVariantParam()
-    // set the variant parameter as a query param in the URL
     parsedUrl.searchParams.set(Personalize.VARIANT_QUERY_PARAM, variantParam)
-    // rewrite the request with the modified URL
     const modifiedRequest = new Request(parsedUrl.toString(), request)
     const response = await fetch(modifiedRequest)
-    return response
+    const modifiedResponse = new Response(response.body, response)
+    // add cookies to the response
+    await Personalize.addStateToResponse(modifiedResponse)
+    // ensure that the response is not cached on the browser
+    modifiedResponse.headers.set('cache-control', 'no-store')
+    return modifiedResponse
 }
